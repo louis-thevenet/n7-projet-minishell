60c6c9833d0a3b5e4a2f22ce90927b49ece0c558
Louis Thevenet <louis.tvnt@gmail.com>
2024-03-25 08:22:00 +0100

    style: format files

--- minishell.c
+++ minishell.c
@@ -1,56 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "readcmd.h"
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(void) {
-    bool fini= false;
+  bool fini = false;
 
-    while (!fini) {
-        printf("> ");
-        struct cmdline *commande= readcmd();
+  while (!fini) {
+    printf("> ");
+    struct cmdline *commande = readcmd();
 
-        if (commande == NULL) {
-            // commande == NULL -> erreur readcmd()
-            perror("erreur lecture commande \n");
+    if (commande == NULL) {
+      // commande == NULL -> erreur readcmd()
+      perror("erreur lecture commande \n");
 
-        } else {
+    } else {
 
-            if (commande->err) {
-                // commande->err != NULL -> commande->seq == NULL
-                printf("erreur saisie de la commande : %s\n", commande->err);
+      if (commande->err) {
+        // commande->err != NULL -> commande->seq == NULL
+        printf("erreur saisie de la commande : %s\n", commande->err);
 
+      } else {
+
+        /* Pour le moment le programme ne fait qu'afficher les commandes
+           tapees et les affiche à l'écran.
+           Cette partie est à modifier pour considérer l'exécution de ces
+           commandes
+        */
+        int indexseq = 0;
+        char **cmd;
+        while ((cmd = commande->seq[indexseq])) {
+          if (cmd[0]) {
+            if (strcmp(cmd[0], "exit") == 0) {
+              fini = true;
+              printf("Au revoir ...\n");
             } else {
-
-                /* Pour le moment le programme ne fait qu'afficher les commandes
-                   tapees et les affiche à l'écran.
-                   Cette partie est à modifier pour considérer l'exécution de ces
-                   commandes
-                */
-                int indexseq= 0;
-                char **cmd;
-                while ((cmd= commande->seq[indexseq])) {
-                    if (cmd[0]) {
-                        if (strcmp(cmd[0], "exit") == 0) {
-                            fini= true;
-                            printf("Au revoir ...\n");
-                        }
-                        else {
-                            printf("commande : ");
-                            int indexcmd= 0;
-                            while (cmd[indexcmd]) {
-                                printf("%s ", cmd[indexcmd]);
-                                indexcmd++;
-                            }
-                            printf("\n");
-                        }
-
-                        indexseq++;
-                    }
-                }
+              printf("commande : ");
+              int indexcmd = 0;
+              while (cmd[indexcmd]) {
+                printf("%s ", cmd[indexcmd]);
+                indexcmd++;
+              }
+              printf("\n");
             }
+
+            indexseq++;
+          }
         }
+      }
     }
-    return EXIT_SUCCESS;
+  }
+  return EXIT_SUCCESS;
 }
--- test_readcmd.c
+++ test_readcmd.c
@@ -1,55 +1,56 @@
 // Visualisation des résultats de readcmd [ZH, le 7/5/21]
 
+#include "readcmd.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "readcmd.h"
 
 int main() {
-    int indexcmd;
-    struct cmdline *commande;
-    printf("==== Visualisation des résultats de readcmd(). Ctrl-C pour finir ===== \n\n");
-    do {
-        printf("Entrez votre commande>>> ");
-        commande = readcmd();
+  int indexcmd;
+  struct cmdline *commande;
+  printf("==== Visualisation des résultats de readcmd(). Ctrl-C pour finir "
+         "===== \n\n");
+  do {
+    printf("Entrez votre commande>>> ");
+    commande = readcmd();
 
-        if (commande == NULL) {
-            printf("\n--- Erreur da la fonction de saisie ou EOF - CtrlD\n");
-        }
-        else {
-            if (commande->err != NULL) {
-                /* tous les autres champs de commande sont NULL */
-                printf("--- Erreur de structure de la commande : %s\n", commande->err);
-            }
+    if (commande == NULL) {
+      printf("\n--- Erreur da la fonction de saisie ou EOF - CtrlD\n");
+    } else {
+      if (commande->err != NULL) {
+        /* tous les autres champs de commande sont NULL */
+        printf("--- Erreur de structure de la commande : %s\n", commande->err);
+      }
 
-            else {
-                if (commande->in != NULL) {
-                    printf("=== Redirection de l'entrée : %s\n", commande->in);
-                }
-                if (commande->out != NULL) {
-                    printf("=== Redirection de la sortie : %s\n", commande->out);
-                }
-                if (commande->backgrounded != NULL) {
-                    printf("=== Commande en tache de fond\n");
-                }
-                /* commande->seq[i] est accessible seulement si :
-                	commande != NULL && command->err == NULL
-                */
-                if (commande->seq[0] == NULL) {
-                    printf("=== Commande vide\n");
-                }
-                else {
-                    int indexseq = 0;
-                    while (commande->seq[indexseq] != NULL) {
-                        printf("\tCommande %d : %s\n", indexseq, commande->seq[indexseq][0]);
-                        indexcmd= 1;
-                        while (commande->seq[indexseq][indexcmd]) {
-                            printf("\tArg %d : %s\n", indexcmd, commande->seq[indexseq][indexcmd]);
-                            indexcmd++;
-                        }
-                        indexseq++;
-                    }
-                }
-            }
+      else {
+        if (commande->in != NULL) {
+          printf("=== Redirection de l'entrée : %s\n", commande->in);
         }
-    } while(1);
+        if (commande->out != NULL) {
+          printf("=== Redirection de la sortie : %s\n", commande->out);
+        }
+        if (commande->backgrounded != NULL) {
+          printf("=== Commande en tache de fond\n");
+        }
+        /* commande->seq[i] est accessible seulement si :
+                commande != NULL && command->err == NULL
+        */
+        if (commande->seq[0] == NULL) {
+          printf("=== Commande vide\n");
+        } else {
+          int indexseq = 0;
+          while (commande->seq[indexseq] != NULL) {
+            printf("\tCommande %d : %s\n", indexseq,
+                   commande->seq[indexseq][0]);
+            indexcmd = 1;
+            while (commande->seq[indexseq][indexcmd]) {
+              printf("\tArg %d : %s\n", indexcmd,
+                     commande->seq[indexseq][indexcmd]);
+              indexcmd++;
+            }
+            indexseq++;
+          }
+        }
+      }
+    }
+  } while (1);
 }

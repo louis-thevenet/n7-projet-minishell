be704940e76749607c07eed4cb45bf08be16a9ee
Louis Thevenet <louis.tvnt@gmail.com>
2024-03-25 08:20:42 +0100

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+
+    # Dev tools
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem =
+        { config
+        , self'
+        , pkgs
+        , lib
+        , system
+        , ...
+        }:
+        {
+          packages.default = pkgs.stdenv.mkDerivation {
+            name = "minishell";
+            version = "1.0";
+
+            src = ./.;
+            nativeBuildInputs = with pkgs; [
+              gccStdenv
+            ];
+
+            buildInputs = with pkgs; [
+            ];
+
+
+            buildPhase = ''
+            '';
+            installPhase = ''
+            '';
+          };
+
+
+
+
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [
+              config.treefmt.build.devShell
+            ];
+
+            packages = with pkgs; [
+              gdb
+              valgrind
+            ];
+          };
+
+
+          treefmt.config = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixpkgs-fmt.enable = true;
+              clang-format.enable = true;
+            };
+          };
+        };
+    };
+}
